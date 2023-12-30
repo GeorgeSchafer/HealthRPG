ab533e66aaf883cf16ddd8281e83c468159d09c8
GeorgeSchafer <77408854+GeorgeSchafer@users.noreply.github.com>
2023-12-30 08:01:09 -0700

    Initial Commit

---
+++ scripts/Fitbit.mjs
@@ -0,0 +1,13 @@
+import ExtEnum from './libs/EnumJS.mjs'
+
+const Zone = new ExtEnum()
+
+function getActivity() {
+    const activity = {
+        zone: Zone.LOW,
+        time: 60, // seconds
+
+    }
+}
+
+
